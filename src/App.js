cdc06743c3a14f771d4c6637aa7bd4fa1c720dac
Winniee356 <lamwingjie@gmail.com>
2022-06-21 20:47:21 +0200

    Create Dictionary search engine component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
       <header className="App-header">
-        
-      <h1>Hello</h1>
+        <main>
+          <Dictionary/>
+        </main>
       </header>
       </div>
     </div>
---
+++ src/Dictionary.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Dictionary() {
+    return <div className="Dictionary" >"Hello from Dictionary" </div>;
+}
