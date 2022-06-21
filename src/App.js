f94b0140f08bb45b39d40eb647f9095171203a5c
Winniee356 <lamwingjie@gmail.com>
2022-06-21 20:29:48 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
+        
       <h1>Hello</h1>
       </header>
+      </div>
     </div>
   );
 }
