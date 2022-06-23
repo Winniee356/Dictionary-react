94f0463711143d9086c208547570aac0bb1acd87
Winniee356 <lamwingjie@gmail.com>
2022-06-23 17:21:15 +0200

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from "./WJLlogo.png";
 import './App.css';
 import Dictionary from "./Dictionary";
 
@@ -6,11 +7,12 @@ function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
         <main>
           <Dictionary/>
         </main>
-      <footer className='text-center'>
+      <footer className='App-footer'>
         <small>
           Coded by Winnie Lam
         </small>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    return <div className="Dictionary" >"Hello from Dictionary" </div>;
+    return (
+    <div className="Dictionary" >
+        <form>
+            <input type="search" />
+        </form>
+    </div>
+    );
 }
