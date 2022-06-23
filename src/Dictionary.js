f8d2171d281e7b6ab20a46d46fd636ac624c6820
Winniee356 <lamwingjie@gmail.com>
2022-06-23 18:10:55 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
    function search(event) {
     event.preventDefault();
     alert ("searching");
    }
    
-   
-   
+   function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+   }
+
     return (
     <div className="Dictionary" >
         <form onSubmit={search}>
-            <input type="search" />
+            <input type="search" onChange={handleKeywordChange}/>
         </form>
     </div>
     );
