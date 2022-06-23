10f124f91e18cc1e2ecacb6aa64ddfba3222a657
Winniee356 <lamwingjie@gmail.com>
2022-06-23 17:23:48 +0200

    Adding Search bar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+   function search(event) {
+    event.preventDefault();
+    alert ("searching");
+   }
+   
+   
+   
     return (
     <div className="Dictionary" >
-        <form>
+        <form onSubmit={search}>
             <input type="search" />
         </form>
     </div>
