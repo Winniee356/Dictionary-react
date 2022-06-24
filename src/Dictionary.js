088000a0c7f363b4005be214f48d039239d94bc8
Winniee356 <lamwingjie@gmail.com>
2022-06-24 22:06:32 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,11 +5,20 @@ import "./Dictionary.css";
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response);
+    }
+
    function search(event) {
     event.preventDefault();
     alert (`searching for ${keyword}`);
-   }
    
+   
+   let apiUrl = "  https://api.dictionaryapi.dev/api/v2/entries/en/watermelon";
+   axios.get(apiUrl).then(handleResponse);
+
+   }
+
    function handleKeywordChange(event) {
     setKeyword(event.target.value);
    }
