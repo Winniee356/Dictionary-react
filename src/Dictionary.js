62e1d982f1e5cd735b14d12bd647ecd008d5b17f
Winniee356 <lamwingjie@gmail.com>
2022-06-24 22:09:50 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
 
     function handleResponse(response) {
-        console.log(response.data);
+        console.log(response.data[0]);
     }
 
    function search(event) {
