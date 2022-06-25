a7462c841931276e38ec6f38661998df1370c1c5
Winniee356 <lamwingjie@gmail.com>
2022-06-25 22:21:49 +0200

    Fix error Results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,7 +5,7 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
-    let [results, setResults] = useState({});
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
         console.log(response.data[0]);
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function Results(props) {
-    console.log(props.results);
+   if (props.results) {
     return <div className="Results" >Hello from Results </div>; 
+  } else {
+    return null;
+  }
 }
