8fc3a3c13f7ff839b0283e56668909d0adcaf0cd
Winniee356 <lamwingjie@gmail.com>
2022-06-25 21:47:04 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-export default function Results() {
+export default function Results(props) {
+    console.log(props.results);
     return <div className="Results" >Hello from Results </div>; 
 }
