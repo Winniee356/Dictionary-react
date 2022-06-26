d0a72a591d313d445eb52fd1d5ffec82132dafcd
Winniee356 <lamwingjie@gmail.com>
2022-06-26 18:31:25 +0200

    looping through arrays of meaning with map

--- src/Results.js
+++ src/Results.js
@@ -5,6 +5,9 @@ export default function Results(props) {
     return (
     <div className="Results">
       <h2>{props.results.word}</h2>
+      {props.results.meanings.map(function(meaning, index){
+        return meaning.definitions[0].definition;
+      })}
       </div>
     ); 
   } else {
