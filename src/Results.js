0fe20e780e52eee4bd4cf8e1b0216e424aaf4b80
Winniee356 <lamwingjie@gmail.com>
2022-06-26 13:49:12 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function Results(props) {
    if (props.results) {
-    return <div className="Results" >Hello from Results </div>; 
+    return (
+    <div className="Results">
+      <h2>{props.results.word}</h2>
+      </div>
+    ); 
   } else {
     return null;
   }
