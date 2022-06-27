c6159583a7c4618f0a6a644e6327cdb08ddc7e2d
Winniee356 <lamwingjie@gmail.com>
2022-06-27 22:27:54 +0200

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,9 +2,8 @@ import React from "react";
 
 export default function Synonyms(props) {
     if (props.synonyms) {
-        return "Hello from Synonyms";
+        return props.synonyms;
     } else {
         return null;
     }
-    
 }
