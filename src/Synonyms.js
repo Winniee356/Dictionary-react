9d47e814dbe44092dbb5d7fe4a32521b934526bb
Winniee356 <lamwingjie@gmail.com>
2022-06-27 22:43:21 +0200

    Update Synonyms.js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,8 +4,8 @@ export default function Synonyms(props) {
     if (props.synonyms) {
         return (
             <ul className="Synonyms">
-            {props.synonyms.map(function(synonyms, index) {
-                return <span key={index}>{synonyms}</span>
+            {props.synonyms.map(function(synonym, index) {
+                return <span key={index}>{synonym}</span>
             })}
             </ul>
         );
