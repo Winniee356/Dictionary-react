fb04c71871de14def758da11febec1a46c640502
Winniee356 <lamwingjie@gmail.com>
2022-06-29 18:19:55 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -28,10 +28,13 @@ export default function Dictionary() {
 
     return (
     <div className="Dictionary" >
+        <section>
         <form onSubmit={search}>
             <input type="search" onChange={handleKeywordChange}/>
         </form>
+        </section>
         <Results results={results}/>
+      
     </div>
     );
 }
