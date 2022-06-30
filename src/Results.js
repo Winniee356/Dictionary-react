328cd386e5bf4a605acbe22ee6e0cbb1489ecbc9
Winniee356 <lamwingjie@gmail.com>
2022-06-30 22:46:44 +0200

    Css style results

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import "./Results.css";
 
 export default function Results(props) {
    if (props.results) {
