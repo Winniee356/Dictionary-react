1e11d32618c3fcd8e0d8b3c8c2cc666facea5fcb
Winniee356 <lamwingjie@gmail.com>
2022-07-01 15:41:15 +0200

    Css button

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
     return (
         <div className="Phonetic">
             < a href={props.phonetic.audio} target="_blank" rel="noreferrer"> Listen </a>
-            {props.phonetic.text}
+            <span className="text">{props.phonetic.text}</span>
         </div>
     );
 }
