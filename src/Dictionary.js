823097aaf7d8ba33e4e1ea52cdc1799deff0ddb4
Winniee356 <lamwingjie@gmail.com>
2022-07-01 17:34:24 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -40,7 +40,7 @@ export default function Dictionary(props) {
         <section>
             <h1>What word do you want to look up Today?</h1>
         <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange}/>
+            <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
         </form>
         <div className="hint">
             Suggested words: Twilight, Thorn, Peanut, Bond
