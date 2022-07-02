a98c2d71da183f0484ee715780a38e815683ad2a
Winniee356 <lamwingjie@gmail.com>
2022-07-02 15:34:00 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
