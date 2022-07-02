aba2b90aa0748907678fa863866bc9ec31c57d29
Winniee356 <lamwingjie@gmail.com>
2022-07-02 15:12:04 +0200

    Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,13 +7,14 @@ export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
     function handleDictionaryResponse(response) {
         setResults(response.data[0]);
     }
 
     function handlePexelsResponse(response) {
-        console.log(response);
+        setPhotos(response.data.photos);
     }
 
     function search() {
@@ -56,6 +57,7 @@ export default function Dictionary(props) {
         </div>
         </section>
         <Results results={results}/>
+        <Photos photos={photos} />
         </div>
     );
     } else {
