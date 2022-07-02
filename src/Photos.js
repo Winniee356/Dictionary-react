56b7225351d7f1e259287cbc062dc3c20f59cae4
Winniee356 <lamwingjie@gmail.com>
2022-07-02 16:20:00 +0200

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -1,13 +1,20 @@
 import React from "react";
 
+
 export default function Photos(props) {
     console.log(props.photos);
     if (props.photos) {
         return (
             <section className="Photos" >
+                <div className="row" >
                 {props.photos.map(function(photo, index) {
-                    return <img src={photo.src.landscape} key={index} />;  
+                    return (
+                        <div className="col-4" key={index} >
+                    <img src={photo.src.landscape} className="img-fluid" />
+                    </div>
+                     ) 
                     })}
+                </div>
             </section>
         );
     } else {
