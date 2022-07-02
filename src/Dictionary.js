f9987b681b53e8ff9a0fcc52316822ab33c8439a
Winniee356 <lamwingjie@gmail.com>
2022-07-02 17:43:32 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary(props) {
     axios.get(apiUrl).then(handleDictionaryResponse);
 
     let pexelsApiKey = "563492ad6f917000010000019313d3c3d84d49debcce0b884cddeeb6";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
     let headers = {Authorization: `Bearer ${pexelsApiKey}`};
     axios
     .get(pexelsApiUrl, { headers: headers, }).then(handlePexelsResponse);
